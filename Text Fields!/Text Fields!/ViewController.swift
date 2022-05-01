906080d556e45d811676b5213fe92dccd7163ebc
DmitriyFod <95651251+DmitriyFod@users.noreply.github.com>
2022-05-01 16:00:08 +0300

    v2

--- Text Fields!/Text Fields!/ViewController.swift
+++ Text Fields!/Text Fields!/ViewController.swift
@@ -12,19 +12,32 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var thirdTxtField: UITextField!
     @IBOutlet weak var fourthTxtField: UITextField!
     @IBOutlet weak var fifthTxtField: UITextField!
+    @IBOutlet weak var currentNumberOfChars: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         firstTxtField.delegate = self
-        //secondTxtField.delegate = self
+        secondTxtField.delegate = self
+        thirdTxtField.delegate = self
         
     }
     //MARK:- UITextFieldDelegate
-    internal func textField(_ firstTxtField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String)->Bool {
-        let setOfLetters = CharacterSet (charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLKMNOPQRSTUVWXYZ ")
-        let typedCharacterSet = CharacterSet(charactersIn: string)
-        return setOfLetters.isSuperset(of: typedCharacterSet)
+    internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String)->Bool {
+        if textField == firstTxtField {
+            let setOfLetters = CharacterSet (charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLKMNOPQRSTUVWXYZ ")
+            let typedCharacterSet = CharacterSet(charactersIn: string)
+            return setOfLetters.isSuperset(of: typedCharacterSet)
+        }else if textField == secondTxtField {
+            let currentText = secondTxtField.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else {
+                return false
+            }//RANGE
+            let updateValue = currentText.replacingCharacters(in: stringRange, with: string)//
+            currentNumberOfChars.text = "\(10 - updateValue.count)"
+            return updateValue.count - 1 < 10
+        }else if textField == thirdTxtField {
+            
         }
-    
+        return true
     }
-
-
+    
+}
