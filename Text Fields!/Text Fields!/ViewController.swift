67e48b57306d8eead51a8e4cd755eaf27212b5d9
DmitriyFod <95651251+DmitriyFod@users.noreply.github.com>
2022-05-04 22:23:16 +0300

    v2

--- Text Fields!/Text Fields!/ViewController.swift
+++ Text Fields!/Text Fields!/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import WebKit
+
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var firstTxtField: UITextField!
     @IBOutlet weak var secondTxtField: UITextField!
@@ -13,11 +15,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var fourthTxtField: UITextField!
     @IBOutlet weak var fifthTxtField: UITextField!
     @IBOutlet weak var currentNumberOfChars: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
         firstTxtField.delegate = self
         secondTxtField.delegate = self
         thirdTxtField.delegate = self
+        fourthTxtField.delegate = self
+        fifthTxtField.delegate = self
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 3)
+        var informationLabel = UILabel()
+        informationLabel.frame = CGRect(x: 0, y: 0, width: 138, height: 88)
+        informationLabel.backgroundColor = .white
+        self.progressBar.setProgress(0, animated: true)
         
     }
     //MARK:- UITextFieldDelegate
@@ -33,11 +43,83 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }//RANGE
             let updateValue = currentText.replacingCharacters(in: stringRange, with: string)//
             currentNumberOfChars.text = "\(10 - updateValue.count)"
-            return updateValue.count - 1 < 10
+            if updateValue.count > 10 {
+                secondTxtField.layer.borderWidth = 1.0
+                secondTxtField.layer.cornerRadius = 8.0
+                secondTxtField.layer.borderColor = UIColor.red.cgColor
+                secondTxtField.textColor = UIColor.red
+                currentNumberOfChars.textColor = UIColor.red
+            }else {
+                secondTxtField.layer.borderColor = UIColor.black.cgColor
+                secondTxtField.textColor = UIColor.black
+                currentNumberOfChars.textColor = UIColor.black
+            }
+            return updateValue.count < 100
         }else if textField == thirdTxtField {
+            guard let thirdText = thirdTxtField.text else {return false}
+            let setOfLetters = CharacterSet (charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLKMNOPQRSTUVWXYZ -")
+            let typedCharacterSet = CharacterSet(charactersIn: string)
+            let allowedNumbers = CharacterSet (charactersIn: "+1234567890")
+            if thirdText.contains("-") {
+                return allowedNumbers.isSuperset(of: typedCharacterSet)
+            }
+            for ch in thirdText {
+                if ch != "-" {
+                    return setOfLetters.isSuperset(of: typedCharacterSet)
+                }
+            }
+        }else if textField == fourthTxtField {
+
+            let urlLink = fourthTxtField.text
             
+            guard let urlLink = urlLink else {
+                return false
+            }
+            guard let url = URL(string: urlLink) else {return true}
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+                UIApplication.shared.open(url)
+            }
+        }else if textField == fifthTxtField {
+                guard let fifthText = fifthTxtField.text else {return false}
+                    if fifthText.count < 8
+                    {
+                        print( "Password must be at least 8 characters")
+                    }
+                    if containsDigit(fifthText)
+                    {
+                        print( "Password must contain at least 1 digit")
+                    }
+                    if containsLowerCase(fifthText)
+                    {
+                        print( "Password must contain at least 1 lowercase character")
+                    }
+                    if containsUpperCase(fifthText)
+                    {
+                        print( "Password must contain at least 1 uppercase character")
+                        
+                    }
+                func containsDigit(_ value: String) -> Bool
+                {
+                    let reqularExpression = ".*[0-9]+.*"
+                    let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+                    return !predicate.evaluate(with: value)
+                }
+                
+                func containsLowerCase(_ value: String) -> Bool
+                {
+                    let reqularExpression = ".*[a-z]+.*"
+                    let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+                    return !predicate.evaluate(with: value)
+                }
+                
+                func containsUpperCase(_ value: String) -> Bool
+                {
+                    let reqularExpression = ".*[A-Z]+.*"
+                    let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+                    return !predicate.evaluate(with: value)
+                }
+
         }
         return true
     }
-    
 }
