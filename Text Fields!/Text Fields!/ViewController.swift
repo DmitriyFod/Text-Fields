b9026cdf88f5df34b1add103dac24f4cc9bb0cfc
DmitriyFod <95651251+DmitriyFod@users.noreply.github.com>
2022-05-12 20:06:34 +0300

    vfinal

--- Text Fields!/Text Fields!/ViewController.swift
+++ Text Fields!/Text Fields!/ViewController.swift
@@ -10,6 +10,12 @@ import WebKit
 import Foundation
 
 class ViewController: UIViewController, UITextFieldDelegate {
+    enum PasswordStrength: CaseIterable {
+        case minimumSymbols
+        case minimum1Digit
+        case minimum1LowerCase
+        case minimum1UpperCase
+    }
     @IBOutlet weak var firstTxtField: UITextField!
     @IBOutlet weak var secondTxtField: UITextField!
     @IBOutlet weak var thirdTxtField: UITextField!
@@ -21,6 +27,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var secondValidationLabel: UILabel!
     @IBOutlet weak var thirdValidationLabel: UILabel!
     @IBOutlet weak var fourthValidationLabel: UILabel!
+    var progresSet = Set<PasswordStrength> ()
     override func viewDidLoad() {
         super.viewDidLoad()
         firstTxtField.delegate = self
@@ -37,7 +44,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     @objc func urlOpen(textField: UITextField) -> Bool{
         let urlLink = fourthTxtField.text
-        
         guard let urlLink = urlLink else {
             return false
         }
@@ -53,9 +59,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
             return setOfLetters.isSuperset(of: typedCharacterSet)
         }else if textField == secondTxtField {
             let currentText = secondTxtField.text ?? ""
-            guard let stringRange = Range(range, in: currentText) else {
-                return false
-            }//RANGE
+            guard let stringRange = Range(range, in: currentText) else {return false}
             let updateValue = currentText.replacingCharacters(in: stringRange, with: string)//
             currentNumberOfChars.text = "\(10 - updateValue.count)"
             if updateValue.count > 10 {
@@ -64,7 +68,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 secondTxtField.layer.borderColor = UIColor.red.cgColor
                 secondTxtField.textColor = UIColor.red
                 currentNumberOfChars.textColor = UIColor.red
-            }else {
+            } else {
                 secondTxtField.layer.borderColor = UIColor.black.cgColor
                 secondTxtField.textColor = UIColor.black
                 currentNumberOfChars.textColor = UIColor.black
@@ -84,13 +88,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 }
             }
         }else if textField == fourthTxtField {
-            
             let urlLink = fourthTxtField.text
-            
             guard let urlLink = urlLink else {
                 return false
             }
-            
             if urlLink.contains("https") {
                 NSObject.cancelPreviousPerformRequests(
                     withTarget: self,
@@ -104,66 +105,70 @@ class ViewController: UIViewController, UITextFieldDelegate {
             }
         }else if textField == fifthTxtField {
             guard let fifthText = fifthTxtField.text else {return false}
-            if fifthText.count >= 7
-            {
-                print( "Password must be at least 8 characters")
+            if fifthText.count >= 7 {
+                progresSet.insert(.minimumSymbols)
                 firstValidationLabel.text = "✓ minimum of 8 characters."
                 firstValidationLabel.textColor = UIColor.green
-
-            }else {
+            } else {
+                progresSet.remove(.minimumSymbols)
                 firstValidationLabel.text = "- minimum of 8 characters."
                 firstValidationLabel.textColor = UIColor.black
-
             }
-            if containsDigit(fifthText)
-            {
-                print( "Password must contain at least 1 digit")
+            if containsDigit(fifthText) {
+                progresSet.insert(.minimum1Digit)
                 secondValidationLabel.text = "✓ minimum 1 digit."
                 secondValidationLabel.textColor = UIColor.green
-            }else {
+            } else {
+                progresSet.remove(.minimum1Digit)
                 secondValidationLabel.text = "- minimum 1 digit."
                 secondValidationLabel.textColor = UIColor.black
             }
-            if containsLowerCase(fifthText)
-            {
-                print( "Password must contain at least 1 lowercase character")
+            if containsLowerCase(fifthText) {
+                progresSet.insert(.minimum1LowerCase)
                 thirdValidationLabel.text = "✓ minimum 1 lowercase."
                 thirdValidationLabel.textColor = UIColor.green
-            }else {
+            } else {
+                progresSet.remove(.minimum1LowerCase)
                 thirdValidationLabel.text = "- minimum 1 lowercase."
                 thirdValidationLabel.textColor = UIColor.black
             }
-            if containsUpperCase(fifthText)
-            {
-                print( "Password must contain at least 1 uppercase character")
+            if containsUpperCase(fifthText) {
+                progresSet.insert(.minimum1UpperCase)
                 fourthValidationLabel.text = "✓ minimum 1 uppercase."
                 fourthValidationLabel.textColor = UIColor.green
-            }else {
+            } else {
+                progresSet.remove(.minimum1UpperCase)
                 fourthValidationLabel.text = "- minimum 1 uppercase."
                 fourthValidationLabel.textColor = UIColor.black
             }
-            func containsDigit(_ value: String) -> Bool
-            {
-                let reqularExpression = ".*[0-9]+.*"
-                let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
-                return predicate.evaluate(with: value)
-            }
+            progressBar.progress = Float((1.0 / Double(PasswordStrength.allCases.count)) * Double(progresSet.count))
             
-            func containsLowerCase(_ value: String) -> Bool
-            {
-                let reqularExpression = ".*[a-z]+.*"
-                let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
-                return predicate.evaluate(with: value)
+            if progressBar.progress  == 0.25 {
+                progressBar.tintColor = UIColor.red
+            } else if progressBar.progress == 1{
+                progressBar.tintColor = UIColor.green
+            } else  {
+                progressBar.tintColor = UIColor.orange
             }
-            
-            func containsUpperCase(_ value: String) -> Bool
-            {
-                let reqularExpression = ".*[A-Z]+.*"
-                let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
-                return predicate.evaluate(with: value)
-            }
-            
         }
         return true
+    }
+    
+    func containsDigit(_ value: String) -> Bool {
+        let reqularExpression = ".*[0-9]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        return predicate.evaluate(with: value)
+    }
+    
+    func containsLowerCase(_ value: String) -> Bool {
+        let reqularExpression = ".*[a-z]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        return predicate.evaluate(with: value)
+    }
+    
+    func containsUpperCase(_ value: String) -> Bool {
+        let reqularExpression = ".*[A-Z]+.*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
+        return predicate.evaluate(with: value)
     }
 }
--- Text Fields!/Text Fields!UITests/Text_Fields_UITests.swift
+++ Text Fields!/Text Fields!UITests/Text_Fields_UITests.swift
@@ -22,7 +22,6 @@ class Text_Fields_UITests: XCTestCase {
         secondTF.tap()
         secondTF.typeText("Long String")
         XCTAssert(app.staticTexts["-1"].exists)
-        //XCTAssertEqual(secondTF.colorWells, UIColor.red)
         let thirdTF = app.textFields["thirdT"]
         thirdTF.tap()
         thirdTF.typeText("abcde-madasda12345")
@@ -39,23 +38,19 @@ class Text_Fields_UITests: XCTestCase {
             XCUIApplication().activate()
             sleep(1)
         }
+        let progBar = app.progressIndicators["currentProgress"]
         let fifthTF = app.textFields["fifthT"]
         fifthTF.tap()
         fifthTF.typeText("qdhlyr159@Aa")
+        sleep(1)
+//        let currentValue = progBar.value as? String
+        XCTAssertTrue(progBar.exists)
+//        XCTAssertEqual(currentValue, "100 %")
         XCTAssert(app.staticTexts["✓ minimum of 8 characters."].exists)
         XCTAssert(app.staticTexts["✓ minimum 1 digit."].exists)
         XCTAssert(app.staticTexts["✓ minimum 1 lowercase."].exists)
         XCTAssert(app.staticTexts["✓ minimum 1 uppercase."].exists)
-        
     }
-        
-        //let request = try XCTUnwrap(fourthTF.value as!
-//        let pr = app.progressIndicators["currentProgress"]
-//        let fifthTF = app.textFields["fifthT"]
-//        firstTF.tap()
-        //fourthTF.typeText("k")
-        //XCTAssertEqual(, 0.25)
-
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
